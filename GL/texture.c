e5bdb8ada39568fa87ed0ed4c82ec26b9aed99d0
Bruce Leet <103404050+Bruceleeto@users.noreply.github.com>
2025-08-16 00:51:19 +0100

    fastcpy issue
    forgot the issue with the but remember using memcpy bypassed. think i fixed what triggered this in my nuquake repo.

--- GL/texture.c
+++ GL/texture.c
@@ -1837,7 +1837,7 @@ void APIENTRY glTexImage2D(GLenum target, GLint level, GLint internalFormat,
             }
         }
 
-        FASTCPY(targetData, conversionBuffer, destBytes);
+        memcpy(targetData, conversionBuffer, destBytes); //FASTCPY
         free(conversionBuffer);
     } else {
         /* No conversion necessary, we can just upload data directly */
